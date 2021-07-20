2f61e1815fe67dd936f773234d6bbd34b509d665
Leandro Pessini <leandro@pessini.me>
2021-07-20 19:57:22 +0100

    Update dashboard.py

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pymongo
+from pymongo import MongoClient, collection
 import json
-from math import ceil
+from urllib.parse import quote_plus
 
 st.set_page_config(
      page_title="Ex-stream-ly Cool App",
@@ -39,32 +39,25 @@ st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-CONN_URI = "mongodb://test-user:test1234@localhost:27017/?authSource=test&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-page_size = 1000
+CONN_URI = quote_plus("mongodb://superuser:anq2677@@localhost:27017/?authSource=test&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+#cache the function and map the type to the hash function
+@st.cache(hash_funcs={MongoClient: id})
+def mongo_connect(url):
+    return MongoClient(url)
 
-def provide_db_connection(func):
-    #@wraps(func)
-    def wrapper(*args, **kwargs):
-        conn = pymongo.MongoClient(CONN_URI)
-        result =  func(conn=conn, *args, **kwargs)
-        conn.close()
-        return result
-    return wrapper
-
-@st.cache
-@provide_db_connection
-def load_mongo_data(conn, db_name, coll_name, page):
-    data = list(conn[db_name][coll_name].find().skip((page-1)*page_size).limit(page_size))
-    return data
+client = mongo_connect(CONN_URI)
+# db = client.mobybikes
 
 
-# coll_name = st.sidebar.selectbox ("Select collection: ",db.list_collection_names())
+# @st.cache(ttl=600)
+def get_data():
+    db = client.mobybikes
+    items = db['moby-historical'].find(limit=5)
+    # items = list(items)  # make hashable for st.cache
+    st.write(items)
+    return items
+items = get_data()
 
-# document_count = db[coll_name].count_documents()
-# page_number = st.number_input(
-#     label="Page Number",
-#     min_value=1,
-#     max_value=ceil(document_count /page_size),
-#     step=1,
-# )
-# st.write(load_mongo_data(coll_name, page_number))
+# Print results.
+for item in items:
+    st.write(f"{item['HarvestTime']} has a :{item['LastRentalStart']}:")
