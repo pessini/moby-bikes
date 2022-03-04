7e11695c108e1ea51036c899d80e68e1426d4140
Leandro Pessini <leandro@pessini.me>
2022-03-04 00:32:09 +0000

    Update inspect_data.py

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -27,30 +27,36 @@ import json
 from pandas import json_normalize
 from datetime import date, datetime
 
+# %%
 # parameters = {
 #     "start": '2020-10-13',
 #     "end": '2021-6-4',
 #     "bikeid": 5
 # }
-parameters = None
+#parameters = None
+parameters = {
+    "start": '2022-03-03',
+    "end": '2022-03-04'
+}
 
 headers={'content-type': 'application/json', 'accept': 'application/json'}
 baseurl = "https://data.smartdublin.ie/mobybikes-api/"
-
-# %%
 last_reading_url = f'{baseurl}last_reading/'
 
-r = requests.get(last_reading_url, headers=headers, params=parameters)
+historical_url = f'{baseurl}historical/'
+
+r = requests.get(historical_url, headers=headers, params=parameters)
 data = r.text
 parse_json = json.loads(data)
-print(parse_json)
+#print(parse_json)
 
 now = datetime.now()
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
-json_filename = f'{dt_string}.json'
-with open("../data/moby.json.csv", 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+json_filename = f'../data/{dt_string}.json'
+json_filename_yesterday = '../data/yesterday2.json'
+with open(json_filename_yesterday, 'w', encoding='utf-8') as f:
+    json.dump(parse_json, f, ensure_ascii=False, indent=4)
     
 
 # %%
