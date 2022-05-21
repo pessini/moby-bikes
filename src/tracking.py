448ca3488d6102e22fd5f9bee66c44c4927f88a8
Leandro Pessini <leandro@pessini.me>
2022-05-21 15:41:57 +0100

    feat: implement classes to track the experiments and export to csv/excel

--- src/tracking.py
+++ src/tracking.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import datetime
+from typing import Union
 
 class Score:
     def __init__(self, metric_name, train, validation=None, test=None):
@@ -10,19 +11,28 @@ class Score:
         self.test = test
 
     def __repr__(self) -> str:
-        return f"Metric: {self.metric_name} - Train: {self.train} - Val: {self.validation} - Test: {self.test}"
+        # return a string in a dic structure to simplify further manipulations
+        return f"{{ 'metric': {self.metric_name}, 'train': {self.train},  'validation': {self.validation}, 'test': {self.test} }}"
 
 class Experiment:
-    def __init__(self, algo_name, hyperparameters, metric, notes=None):
+    def __init__(self, algorithm, predictors, hyperparameters, score=Union[Score, list], notes=None):
+        # Union[int, float] from Python 3.10 onwards can be replace by "|" (e.g: int | float)
+
+        if(isinstance(score, list)):
+            assert all(isinstance(n, Score) for n in score), "All elements in the list must be a Score object!!!"
+        else:
+            assert isinstance(score, Score), "Metric Score must be a Score object!!!"
+
         #Attributes
-        self.algo_name = algo_name
+        self.algorithm = algorithm
+        self.predictors = predictors
         self.hyperparameters = hyperparameters
-        self.metric = metric
+        self.dict_scores = score
         self.date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         self.notes = notes
         
     def __repr__(self) -> str:
-        return f"Experiment: {self.algo_name} - Datetime: {self.date} - Hyperparamaters: {self.hyperparameters} - Metrics: {self.metric} - Notes: {self.notes}"
+        return f"Experiment: {self.algorithm} - Datetime: {self.date} - Hyperparamaters: {self.hyperparameters} - Score: {self.dict_scores} - Notes: {self.notes}"
         
 class ExperimentTracker:
 
@@ -35,11 +45,11 @@ class ExperimentTracker:
         else:
             raise Exception("Cannot add an experiment which is not an Experiment Object!!!")
 
-    def save(self):
-        columns_name = ['Title','Date','Predictors','Hyperparameters','Metric','Train','Validation','Test','Details']
+    def to_dataframe(self):
+        # columns_name = ['Title','Date','Predictors','Hyperparameters','Metric','Train','Validation','Test','Details']
         data = self.experiments
-        print(data)
-        return pd.DataFrame( [vars(e) for e in data] )
+       # data => List of Experiment()
+        return pd.DataFrame( [vars(e) for e in data])
     
 ##########################################################################################
 
@@ -47,18 +57,30 @@ df_tracking = ExperimentTracker()
 
 ###############################
 
+###############################
+######## Experiment 1 #########
+###############################
 rsme = Score('RSME', '3.2223', '2.2323', '4.5623')
-exp_linr = Experiment('Linear Regression', 'alpha=5', rsme, 'Hello there LN!')
+exp_linr = Experiment('Linear Regression','["temp","rhum","wdsp"]', 'alpha=5', rsme, 'Hello there LN!')
 df_tracking.add_experiment(exp_linr)
 
-mae = Score('MAE', '7482.2', '243.23', '424.523')
-exp_rf = Experiment('Random Forest','num_leafs=5',mae,'Hello there RF!')
+###############################
+######## Experiment 2 #########
+###############################
+exp_linr2 = Experiment('Linear Regression','["temp","rhum","wdsp"]', 'alpha=10', rsme, 'LN! alpha is 10 now')
+df_tracking.add_experiment(exp_linr2)
+
+###############################
+######## Experiment 3 #########
+###############################
+mae_rf = Score('MAE', '7482.2', '243.23', '424.523')
+rsm_rf = Score('RSM', '72.2', '24.23', '4.5')
+metrics_rf = [mae_rf,rsm_rf]
+exp_rf = Experiment('Random Forest', '["temp","rhum","wdsp"]', 'num_leafs = 5',metrics_rf,'Hello there RF!')
 df_tracking.add_experiment(exp_rf)
 
 ##############################################################
-
-print('\n')
-df = df_tracking.save()
-print('\n')
-print(df)
-df.to_csv('experiment.csv')
+df = df_tracking.to_dataframe()
+# print(df)
+df.to_csv('experiment.csv', index=False)
+df.to_excel('tracking.xlsx', sheet_name='ML-Experiment', index=False)
