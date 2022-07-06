f57c6ae69cb931436408a7f5707978b0d67bd179
Leandro Pessini <leandro@pessini.me>
2022-07-06 22:22:07 +0100

    feat: loading data in the dashboard streamlit

---
+++ dashboard/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+from sklearn import pipeline
+import xgboost as xgb
+import pickle
+
+st.write(
+    """
+# Dashboard
+    """
+)
+
+xgb_pipe = pickle.load(open("xgb_pipeline.pkl", "rb"))
+@st.cache
+def predict(df):
+    return xgb_pipe.predict(df)
+
+@st.cache
+def load_data():
+    return pd.read_csv("df_test.csv")
+
+df = load_data()
+st.dataframe(df)
