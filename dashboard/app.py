e77bf1f380fc5ee29f6a9a5b851e411bfd12228d
Leandro Pessini <leandro@pessini.me>
2022-07-10 18:45:57 +0100

    feat: reading weather api and predicting rentals demand

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 import numpy as np
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, OrdinalEncoder
@@ -12,12 +13,13 @@ import category_encoders as ce
 import requests
 from bs4 import BeautifulSoup
 
+@st.cache
 def parse_xml(xml_data):
   # Initializing soup variable
     soup = BeautifulSoup(xml_data, 'lxml')
   # Iterating through time tag and extracting elements
     all_items = soup.find_all('time')
-    forecast_df = pd.DataFrame(columns=['temp', 'wdsp', 'rhum', 'rainfall_intensity'])
+    forecast_df = pd.DataFrame(columns=['temp', 'wdsp', 'rhum', 'rain'])
 
     for item in all_items:
 
@@ -32,7 +34,6 @@ def parse_xml(xml_data):
         if item.location.find('humidity'):
             humidity = item.location.humidity.get('value')
         
-
         row = {
             'temp': temp,
             'rhum': humidity,
@@ -43,14 +44,11 @@ def parse_xml(xml_data):
             precipitation = item.location.precipitation.get('value')
     
         if datetime_object in forecast_df.index:
-            forecast_df.loc[datetime_object]['rainfall_intensity'] = precipitation
+            forecast_df.loc[datetime_object]['rain'] = precipitation
         else:
             forecast_df = pd.concat([pd.DataFrame(row, columns=forecast_df.columns, index=[datetime_object]),forecast_df])
-        
-    # df = pd.DataFrame.from_records(forecast, index=datetime_object)
-    st.write(forecast_df.sort_index())
 
-    return 'df'
+    return forecast_df.sort_index()
 
 # Preprocessing
 def preprocessor(predictors: list) -> ColumnTransformer:
@@ -132,32 +130,107 @@ def preprocessor(predictors: list) -> ColumnTransformer:
                              remainder='drop')
 
 
+def get_season(date: pd.DatetimeIndex) -> str:
+    '''
+        Receives a date and returns the corresponded season
+        0 - Spring | 1 - Summer | 2 - Autumn | 3 - Winter
+        Vernal equinox(about March 21): day and night of equal length, marking the start of spring
+        Summer solstice (June 20 or 21): longest day of the year, marking the start of summer
+        Autumnal equinox(about September 23): day and night of equal length, marking the start of autumn
+        Winter solstice (December 21 or 22): shortest day of the year, marking the start of winter
+    '''
+    Y = 2000 # dummy leap year to allow input X-02-29 (leap day)
+    seasons = [('Winter', (datetime(Y,  1,  1),  datetime(Y,  3, 20))),
+            ('Spring', (datetime(Y,  3, 21),  datetime(Y,  6, 20))),
+            ('Summer', (datetime(Y,  6, 21),  datetime(Y,  9, 22))),
+            ('Autumn', (datetime(Y,  9, 23),  datetime(Y, 12, 20))),
+            ('Winter', (datetime(Y, 12, 21),  datetime(Y, 12, 31)))]
+    date = date.replace(year=Y)
+    return next(season for season, (start, end) in seasons if start <= date <= end)
+
+def rain_intensity_level(rain: float) -> str:
+    '''
+    Receives a rain intensity (in mm) and returns the rain intensity level
+    '''
+    conditions = [
+        (rain == 0.0), # no rain
+        (rain <= 0.3), # drizzle
+        (rain > 0.3) & (rain <= 0.5), # light rain
+        (rain > 0.5) & (rain <= 4), # moderate rain
+        (rain > 4) # heavy rain
+        ]
+    values = ['no rain', 'drizzle', 'light rain', 'moderate rain','heavy rain']
+    
+    return np.select(conditions, values)
+
+def get_day_of_week_number(date: pd.DatetimeIndex) -> int:
+    return date.dayofweek
+
+def get_day_of_week_str(date: pd.DatetimeIndex) -> str:
+    return date.day_name()
+
+def isWorkingDay(date: pd.DatetimeIndex) -> bool:
+    return (get_day_of_week_number(date) < 5)
+
+def times_of_day(hour: int) -> str:
+    '''
+    Receives an hour and returns the time of day
+    Morning: 7:00 - 11:59
+    Afternoon: 12:01 - 17:59
+    Evening: 18:00 - 22:59
+    Night: 23:00 - 06:59
+    '''
+    conditions = [
+        (hour < 7), # night 23:00 - 06:59
+        (hour >= 7) & (hour < 12), # morning 7:00 - 11:59
+        (hour >= 12) & (hour < 18), # afternoon 12:01 - 17:59
+        (hour >= 18) & (hour < 23) # evening 18:00 - 22:59
+    ]
+    values = ['Night', 'Morning', 'Afternoon', 'Evening']
+    
+    return np.select(conditions, values,'Night')
+
+def round_up(x):
+    '''
+    Helper function to round away from zero
+    '''
+    from math import copysign
+    return int(x + copysign(0.5, x))
+
+@st.cache(allow_output_mutation=True)
+def generate_features(df):
+    
+    df['hour'] = df.index.hour
+    df['date'] = pd.to_datetime(arg=df.index, utc=True, infer_datetime_format=True).date
+    
+    # day of the week
+    df['dayofweek_n'] = df.index.map(get_day_of_week_number)
+    df['dayofweek'] = df.index.map(get_day_of_week_str)
+
+    # working day (Monday=0, Sunday=6)
+    # from 0 to 4 or monday to friday and is not holiday
+    df['working_day'] = df.index.map(isWorkingDay)
+    df['timesofday'] = df['hour'].map(times_of_day)
+    df['season'] = pd.to_datetime(df['date']).map(get_season)
+    
+    df['rainfall_intensity'] = pd.to_numeric(df['rain']).map(rain_intensity_level)
+
+    return df
+
 st.header('Dashboard')
 st.subheader("Predicting bike rentals demand")
 # predictors = ['temp','rhum','dayofweek','timesofday','wdsp','rainfall_intensity', 'working_day', 'hour', 'season']
 
 xgb_pipe = pickle.load(open("../models/xgb_pipeline.pkl", "rb"))
 
-@st.cache
+# @st.cache
 def predict(df):
     return xgb_pipe.predict(df)
-
-@st.cache
-def load_data():
-    df = pd.read_csv("df_test.csv")
-    X = df.drop(['count'], axis=1)
-    y = df.pop('count')
-    return X,y
 
 st.write('''
          #### Normalized Root Mean Square Error (NRMSE)
          ''')
 st.latex(r'''NRMSE = \frac{RSME}{y_{max} - y_{min}}''')
-
-X,y = load_data()
-predicted = pd.Series(xgb_pipe.predict(X))
-
-st.dataframe(predicted)
 
 
 st.write('''
@@ -169,7 +242,10 @@ Latitude: 53.4264 - Longitude: -6.2499 \n
 URL_WEATHER_API = "http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=53.4264;long=-6.2499"
 response = requests.get(URL_WEATHER_API).content
 
-# st.write(soup.prettify())
+df_xml = parse_xml(response)
+df_forecast = generate_features(df_xml)
 
-df_forecast = parse_xml(response)
-
+predicted = pd.Series( xgb_pipe.predict(df_forecast), name='predicted') # round up to nearest integer
+predicted = predicted.map(round_up)
+df_forecast['predicted'] = predicted.values
+st.dataframe(df_forecast)
