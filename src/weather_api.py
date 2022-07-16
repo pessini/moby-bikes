91b595234d0fd21f7d535ab3d5c7347e6fc280eb
Leandro Pessini <leandro@pessini.me>
2022-07-16 22:25:38 +0100

    feat: creates a txt file if api request fails

--- src/weather_api.py
+++ src/weather_api.py
@@ -24,14 +24,18 @@ today_dt = datetime.now()
 yesterday_dt = today_dt - timedelta(days=1)
 yesterday_dt_str = yesterday_dt.strftime("%Y-%m-%d")
 
+WEATHER_DATA_URL = 'https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday'
+ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
+
 try:
-    dubairport_yesterday = pd.read_csv('https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday')
+    dubairport_yesterday = pd.read_csv(WEATHER_DATA_URL)
     dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
     dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
     dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
-    ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
     csv_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.csv'
     if not os.path.exists(csv_filename):
-        dubairport_yesterday.to_csv(csv_filename, index=False)
+        dubairport_yesterday.to_csv(csv_filename, index=False)  
 except Exception as e:
-    print(e)
+    txt_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/error_{yesterday_dt_str}.txt'
+    with open(txt_filename, 'w') as f:
+        f.write(e.__str__())
