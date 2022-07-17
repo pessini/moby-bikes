ec3e559fc6ebfe00207328ef5f14d501642c5593
Leandro Pessini <leandro@pessini.me>
2022-07-17 21:02:24 +0100

    sql: create procedure to new rentals

--- documentation/business_rules.sql
+++ documentation/business_rules.sql
@@ -3,24 +3,27 @@ Assumption: Due to lack of information and data, to calculate the duration renta
 	the duration in *minutes* will be calculated by: RentalDuration = LastGPSTime - LastRentalStart
     TIMESTAMPDIFF(unit,datetime_expr1,datetime_expr2)
 */
-
+-- DROP FUNCTION IF EXISTS rental_duration;
 -- DELIMITER //
 -- CREATE FUNCTION rental_duration(last_gpstime datetime, rental_start datetime) 
 -- RETURNS int
 -- BEGIN
 --     RETURN TIMESTAMPDIFF(MINUTE, last_gpstime, rental_start);
--- END;
--- //
+-- END; //
 
 /** 
-Error Code: 1418. This function has none of DETERMINISTIC, NO SQL, or READS SQL DATA in its declaration 
-and binary logging is enabled (you *might* want to use the less safe log_bin_trust_function_creators variable)
+https://www.geeksforgeeks.org/different-types-of-procedures-in-mysql/
+https://www.tutorialspoint.com/How-MySQL-IF-ELSE-statement-can-be-used-in-a-stored-procedure
 */
 DROP PROCEDURE IF EXISTS check_rentals;
 delimiter //
 create procedure check_rentals() 
 begin 
-	select * from mobybikes.tmpRentals order by LastRentalStart ASC; 
+	select 
+		LastRentalStart, BikeID
+    from mobybikes.tmpRentals
+    group by LastRentalStart, BikeID
+    order by BikeID,LastRentalStart asc; 
 end //
 
 call check_rentals(); //
