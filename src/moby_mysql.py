d1a07ef3fded908116bfccd3f23f471c2c580255
Leandro Pessini <leandro@pessini.me>
2022-07-19 22:54:24 +0100

    Changing workspace

--- src/moby_mysql.py
+++ src/moby_mysql.py
@@ -3,33 +3,31 @@
 # User: root
 # SSL: enabled with TLS_AES_256_GCM_SHA384
 
+#%%
 import mysql.connector
 import pandas as pd
 import numpy as np
 
-# config = {
-#   'user': 'root',
-#   'password': 'SanFedele0430@',
-#   'host': '127.0.0.1',
-#   'database': 'mobybikes',
-#   'raise_on_warnings': True
-# }
 config = {
   'user': 'root',
-  'password': 'anq2677@',
+  'password': 'SanFedele0430@',
   'host': '127.0.0.1',
   'database': 'mobybikes',
   'raise_on_warnings': True
 }
+# config = {
+#   'user': 'root',
+#   'password': 'anq2677@',
+#   'host': '127.0.0.1',
+#   'database': 'mobybikes',
+#   'raise_on_warnings': True
+# }
 
 cnx = mysql.connector.connect(**config)
 
 cursor = cnx.cursor()
 
-query = ("select LastRentalStart, BikeID, TIMESTAMPDIFF(MINUTE, MAX(LastGPSTime), LastRentalStart) * -1 as duration "
-         "from mobybikes.tmpRentals "
-         "group by LastRentalStart, BikeID "
-         "having BikeID = 10 order by LastRentalStart asc")
+query = ("select * from mobybikes.tmpRentals order by LastRentalStart asc")
 
 cursor.execute(query)
 myresult = cursor.fetchall()
@@ -38,5 +36,11 @@ df = pd.DataFrame(myresult, columns=cursor.column_names)
 cursor.close()
 cnx.close()
 
-print(df)
+#%%
+df.head()
 
+
+# %%
+df_grouped = df.groupby(['LastRentalStart', 'BikeID'])
+df_grouped.head()
+# %%
