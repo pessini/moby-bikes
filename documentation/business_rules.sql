9967ea5a6013ba6903b6102192ab5587904c3329
Leandro Pessini <leandro@pessini.me>
2022-07-20 22:04:22 +0100

    Changing Workspace

--- documentation/business_rules.sql
+++ documentation/business_rules.sql
@@ -2,27 +2,39 @@
 Assumption: Due to lack of information and data, to calculate the duration rental time I am assuming that when a new bike rental starts 
 	the duration in *minutes* will be calculated by: RentalDuration = LastGPSTime - LastRentalStart
 */
--- DROP FUNCTION IF EXISTS rental_duration;
--- DELIMITER //
--- CREATE FUNCTION rental_duration(last_gpstime datetime, rental_start datetime) 
--- RETURNS int
--- BEGIN
---     RETURN TIMESTAMPDIFF(MINUTE, last_gpstime, rental_start) * -1;
--- END; //
+DROP FUNCTION IF EXISTS rental_duration;
+DROP PROCEDURE IF EXISTS check_rentals;
+
+DELIMITER //
+CREATE FUNCTION rental_duration(last_gpstime datetime, rental_start datetime) 
+RETURNS int
+deterministic
+BEGIN
+    RETURN TIMESTAMPDIFF(MINUTE, last_gpstime, rental_start) * -1;
+END; //
 
 /** 
 https://www.geeksforgeeks.org/different-types-of-procedures-in-mysql/
 https://www.tutorialspoint.com/How-MySQL-IF-ELSE-statement-can-be-used-in-a-stored-procedure
 */
-DROP PROCEDURE IF EXISTS check_rentals;
 delimiter //
-create procedure check_rentals() 
+create procedure check_rentals()
 begin 
+	-- declare bikes int;
+--     declare rentals_dt datetime;
 	select 
-		LastRentalStart, BikeID, LastGPSTime
+		LastRentalStart,
+		BikeID,
+        rental_duration(MAX(LastGPSTime),LastRentalStart) as duration
+        
     from mobybikes.tmpRentals
-    group by LastRentalStart, BikeID, LastGPSTime
-    order by BikeID,LastRentalStart asc; 
+    group by
+		LastRentalStart, BikeID
+    order by LastRentalStart asc;
+    
+    -- insert into 
 end //
+
+
 
 call check_rentals(); //
