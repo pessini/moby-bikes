6ff64e6beb6cef589669900463ce4939b0af6e89
Leandro Pessini <leandro@pessini.me>
2022-08-05 17:36:19 +0100

    feat: if weather json api is unavailable, retrieves csv

--- src/load_files_api.py
+++ src/load_files_api.py
@@ -27,45 +27,44 @@ def format_hour(hour) -> int:
     hour_str = hour.split(':')
     return int(hour_str[0])
 
-# WEATHER_DATA_URL = 'https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday'
-ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
-
-
 headers={'content-type': 'application/json', 'accept': 'application/json'}
-WEATHER_DATA_API_URL = "https://prodapi.metweb.ie/observations/dublin/yesterday"
 
+WEATHER_DATA_API_URL = "https://prodapi.metweb.ie/observations/dublin/yesterday"
+WEATHER_DATA_CSV_URL = 'https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday'
 ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
 
-api_response = requests.get(WEATHER_DATA_API_URL, headers=headers)
-json_str = api_response.text
-parse_json = json.loads(json_str)
-
-if api_response:
-    json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.json'
-else:
-    json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}_error.json'
+try:
     
-if not os.path.exists(json_filename):
-    with open(json_filename, 'w', encoding='utf-8') as f:
-        json.dump(parse_json, f, ensure_ascii=False, indent=4)
+    api_response = requests.get(WEATHER_DATA_API_URL, headers=headers)
+    json_str = api_response.text
+    parse_json = json.loads(json_str)
 
+    if api_response:
+        json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.json'
+    else:
+        json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}_error.json'
 
-
-# try:
+    if not os.path.exists(json_filename):
+        with open(json_filename, 'w', encoding='utf-8') as f:
+            json.dump(parse_json, f, ensure_ascii=False, indent=4)
+            
+except Exception:
     
-#     dubairport_yesterday = pd.read_csv(WEATHER_DATA_URL)
-#     dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
-#     dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
-#     dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
-#     csv_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.csv'
-#     if not os.path.exists(csv_filename):
-#         dubairport_yesterday.to_csv(csv_filename, index=False)  
-# except Exception as e:
-#     txt_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/error_{yesterday_dt_str}.txt'
-#     with open(txt_filename, 'w') as f:
-#         f.write(e.__str__())
+    try:
         
+        dubairport_yesterday = pd.read_csv(WEATHER_DATA_CSV_URL)
+        dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
+        dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
+        dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
+        csv_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.csv'
+        if not os.path.exists(csv_filename):
+            dubairport_yesterday.to_csv(csv_filename, index=False)
+            
+    except Exception as e:
         
+        txt_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/error_{yesterday_dt_str}.txt'
+        with open(txt_filename, 'w') as f:
+            f.write(e.__str__())
 
 ###############################################################
 # Moby Bikes Data
@@ -81,23 +80,19 @@ parameters = {
     "end": today_dt_str
 }
 
-headers={'content-type': 'application/json', 'accept': 'application/json'}
 baseurl = "https://data.smartdublin.ie/mobybikes-api/"
 # last_reading_url = f'{baseurl}last_reading/'
 
 historical_url = f'{baseurl}historical/'
+api_response_moby = requests.get(historical_url, headers=headers, params=parameters)
+moby_json_str = api_response_moby.text
+moby_parse_json = json.loads(moby_json_str)
 
-ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
-
-api_response = requests.get(historical_url, headers=headers, params=parameters)
-json_str = api_response.text
-parse_json = json.loads(json_str)
-
-if api_response:
-    json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}.json'
+if api_response_moby:
+    moby_json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}.json'
 else:
-    json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}_error.json'
-    
-if not os.path.exists(json_filename):
-    with open(json_filename, 'w', encoding='utf-8') as f:
-        json.dump(parse_json, f, ensure_ascii=False, indent=4)
+    moby_json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}_error.json'
+
+if not os.path.exists(moby_json_filename):
+    with open(moby_json_filename, 'w', encoding='utf-8') as f:
+        json.dump(moby_parse_json, f, ensure_ascii=False, indent=4)
