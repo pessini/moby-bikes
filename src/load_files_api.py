044e54971da1f6d7d881bee63672d7aa3182143f
Leandro Pessini <leandro@pessini.me>
2022-08-05 23:29:15 +0100

    feat: reads data from apis and write to a S3 bucket

--- src/load_files_api.py
+++ src/load_files_api.py
@@ -3,6 +3,11 @@ from datetime import datetime, timedelta
 import os
 import requests
 import json
+import boto3
+
+s3_client = boto3.client("s3")
+LOCAL_FILE_SYS = "/tmp"
+S3_BUCKET = "moby-bikes-rentals"
 
 today_dt = datetime.now()
 today_dt_str = today_dt.strftime("%Y-%m-%d")
@@ -17,7 +22,8 @@ yesterday_dt_str = yesterday_dt.strftime("%Y-%m-%d")
 # Download CSV
 # URL: https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday
 
-# API (not working) - 504 Gateway Timeout
+# API (sometimes is not working) - 504 Gateway Timeout 
+# Checking alternative with CSV file
 # URL: https://prodapi.metweb.ie/observations/dublin/yesterday
 
 def format_hour(hour) -> int:
@@ -40,12 +46,12 @@ try:
     parse_json = json.loads(json_str)
 
     if api_response:
-        json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.json'
+        weather_filename = f'{yesterday_dt_str}.json'
     else:
-        json_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}_error.json'
+        weather_filename = f'{yesterday_dt_str}_error.json'
 
-    if not os.path.exists(json_filename):
-        with open(json_filename, 'w', encoding='utf-8') as f:
+    if not os.path.exists(weather_filename):
+        with open(weather_filename, 'w', encoding='utf-8') as f:
             json.dump(parse_json, f, ensure_ascii=False, indent=4)
             
 except Exception:
@@ -56,13 +62,13 @@ except Exception:
         dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
         dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
         dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
-        csv_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/{yesterday_dt_str}.csv'
-        if not os.path.exists(csv_filename):
-            dubairport_yesterday.to_csv(csv_filename, index=False)
+        weather_filename = f'{yesterday_dt_str}.csv'
+        if not os.path.exists(weather_filename):
+            dubairport_yesterday.to_csv(weather_filename, index=False)
             
     except Exception as e:
         
-        txt_filename = f'{ROOT_DIR_LOCAL}/data/external/weather/error_{yesterday_dt_str}.txt'
+        txt_filename = f'{yesterday_dt_str}_error.txt'
         with open(txt_filename, 'w') as f:
             f.write(e.__str__())
 
@@ -89,10 +95,13 @@ moby_json_str = api_response_moby.text
 moby_parse_json = json.loads(moby_json_str)
 
 if api_response_moby:
-    moby_json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}.json'
+    moby_json_filename = f'{yesterday_dt_str}.json'
 else:
-    moby_json_filename = f'{ROOT_DIR_LOCAL}/data/external/moby/{yesterday_dt_str}_error.json'
+    moby_json_filename = f'{yesterday_dt_str}_error.json'
 
 if not os.path.exists(moby_json_filename):
     with open(moby_json_filename, 'w', encoding='utf-8') as f:
         json.dump(moby_parse_json, f, ensure_ascii=False, indent=4)
+
+s3_client.upload_file(LOCAL_FILE_SYS + "/" + weather_filename, S3_BUCKET, "/weather/" + weather_filename)
+s3_client.upload_file(LOCAL_FILE_SYS + "/" + moby_json_filename, S3_BUCKET, "/moby/" + moby_json_filename)
