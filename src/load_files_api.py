af9bb76cca0e70578a408300113345e41c6b5e4b
Leandro Pessini <leandro@pessini.me>
2022-08-07 15:06:18 +0100

    feat: lambda_handler to pull data from api tested

--- src/load_files_api.py
+++ src/load_files_api.py
@@ -37,71 +37,81 @@ headers={'content-type': 'application/json', 'accept': 'application/json'}
 
 WEATHER_DATA_API_URL = "https://prodapi.metweb.ie/observations/dublin/yesterday"
 WEATHER_DATA_CSV_URL = 'https://www.met.ie/latest-reports/observations/download/Dublin-Airport/yesterday'
-ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
+# ROOT_DIR_LOCAL = os.path.abspath(os.curdir)
 
-try:
-    
-    api_response = requests.get(WEATHER_DATA_API_URL, headers=headers)
-    json_str = api_response.text
-    parse_json = json.loads(json_str)
-
-    if api_response:
-        weather_filename = f'{yesterday_dt_str}.json'
-    else:
-        weather_filename = f'{yesterday_dt_str}_error.json'
-
-    if not os.path.exists(weather_filename):
-        with open(weather_filename, 'w', encoding='utf-8') as f:
-            json.dump(parse_json, f, ensure_ascii=False, indent=4)
-            
-except Exception:
-    
+def get_weather_data():
+     
     try:
         
-        dubairport_yesterday = pd.read_csv(WEATHER_DATA_CSV_URL)
-        dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
-        dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
-        dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
-        weather_filename = f'{yesterday_dt_str}.csv'
-        if not os.path.exists(weather_filename):
-            dubairport_yesterday.to_csv(weather_filename, index=False)
-            
-    except Exception as e:
+        api_response = requests.get(WEATHER_DATA_API_URL, headers=headers)
+        json_str = api_response.text
+        parse_json = json.loads(json_str)
+
+        if api_response:
+            weather_filename = f'{yesterday_dt_str}.json'
+        else:
+            weather_filename = f'{yesterday_dt_str}_error.json'
+
+        with open(LOCAL_FILE_SYS + "/" + weather_filename, 'w', encoding='utf-8') as f:
+            json.dump(parse_json, f, ensure_ascii=False, indent=4)
+                
+    except Exception:
         
-        txt_filename = f'{yesterday_dt_str}_error.txt'
-        with open(txt_filename, 'w') as f:
-            f.write(e.__str__())
+        try:
+            
+            dubairport_yesterday = pd.read_csv(WEATHER_DATA_CSV_URL)
+            dubairport_yesterday.columns = ['time', 'report', 'temp', 'wdsp', 'wind_gust', 'wind_direction', 'rain', 'pressure']
+            dubairport_yesterday['time'] = dubairport_yesterday['time'].apply(format_hour)
+            dubairport_yesterday.drop(['report', 'wind_gust', 'wind_direction', 'pressure'], axis=1, inplace=True)
+            weather_filename = f'{yesterday_dt_str}.csv'
+            dubairport_yesterday.to_csv(LOCAL_FILE_SYS + "/" + weather_filename, index=False)
+                
+        except Exception as e:
+            
+            weather_filename = f'{yesterday_dt_str}_error.txt'
+            with open(LOCAL_FILE_SYS + "/" + weather_filename, 'w') as f:
+                f.write(e.__str__())
+        
+    finally:
+        s3_client.upload_file(LOCAL_FILE_SYS + "/" + weather_filename, S3_BUCKET, "weather_" + weather_filename)
+    
+def get_rentals_data():
+    ###############################################################
+    # Moby Bikes Data
+    # https://data.gov.ie/dataset/moby-bikes
+    ###############################################################
 
-###############################################################
-# Moby Bikes Data
-# https://data.gov.ie/dataset/moby-bikes
-###############################################################
+    # Moby API
+    # URL: https://data.smartdublin.ie/mobybikes-api/
 
-# Moby API
-# URL: https://data.smartdublin.ie/mobybikes-api/
+    #parameters = None
+    parameters = {
+        "start": yesterday_dt_str,
+        "end": today_dt_str
+    }
 
-#parameters = None
-parameters = {
-    "start": yesterday_dt_str,
-    "end": today_dt_str
-}
+    baseurl = "https://data.smartdublin.ie/mobybikes-api/"
+    # last_reading_url = f'{baseurl}last_reading/'
 
-baseurl = "https://data.smartdublin.ie/mobybikes-api/"
-# last_reading_url = f'{baseurl}last_reading/'
+    historical_url = f'{baseurl}historical/'
+    api_response_moby = requests.get(historical_url, headers=headers, params=parameters)
+    moby_json_str = api_response_moby.text
+    moby_parse_json = json.loads(moby_json_str)
 
-historical_url = f'{baseurl}historical/'
-api_response_moby = requests.get(historical_url, headers=headers, params=parameters)
-moby_json_str = api_response_moby.text
-moby_parse_json = json.loads(moby_json_str)
+    if api_response_moby:
+        moby_json_filename = f'{yesterday_dt_str}.json'
+    else:
+        moby_json_filename = f'{yesterday_dt_str}_error.json'
 
-if api_response_moby:
-    moby_json_filename = f'{yesterday_dt_str}.json'
-else:
-    moby_json_filename = f'{yesterday_dt_str}_error.json'
-
-if not os.path.exists(moby_json_filename):
-    with open(moby_json_filename, 'w', encoding='utf-8') as f:
+    with open(LOCAL_FILE_SYS + "/" + moby_json_filename, 'w', encoding='utf-8') as f:
         json.dump(moby_parse_json, f, ensure_ascii=False, indent=4)
-
-s3_client.upload_file(LOCAL_FILE_SYS + "/" + weather_filename, S3_BUCKET, "/weather/" + weather_filename)
-s3_client.upload_file(LOCAL_FILE_SYS + "/" + moby_json_filename, S3_BUCKET, "/moby/" + moby_json_filename)
+            
+    s3_client.upload_file(LOCAL_FILE_SYS + "/" + moby_json_filename, S3_BUCKET, "moby_" + moby_json_filename)
+        
+        
+def lambda_handler(event, context):
+    '''
+    Loads the weather data from the API and saves it to S3
+    '''
+    get_weather_data()
+    get_rentals_data()
