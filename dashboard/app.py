8d8aa88061616b1d77f02ad5f85edc6fe4fa6fe5
Leandro Pessini <leandro@pessini.me>
2022-09-07 21:30:05 +0100

    fix: documentation css style

--- dashboard/app.py
+++ dashboard/app.py
@@ -53,10 +53,9 @@ hide_st_style = """
                     background-color: #206b82;
                     text-decoration: none;
                     text-align: center;
-                    background-image: none;
-                    padding: 12px;
+                    padding: 6px 12px 12px 14px; /* top right bottom left */
                     font-size: 16px;
-                    border-radius: 6px;
+                    border-radius: 8px;
                 }
                 a.btn:hover{
                     opacity: 90%;
@@ -593,8 +592,7 @@ if selected == "About":
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
                 <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-            </svg>
-            <span style="vertical-align: baseline;">&nbspRead full documentation</span>
+            </svg><span style="vertical-align: sub;">&nbspRead Documentation</span>
         </a>
     </div>
     '''
