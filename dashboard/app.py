ac15a4c12ffea6b869861d88b5594d3f7653b2c0
Leandro Pessini <leandro@pessini.me>
2022-09-07 15:18:34 +0100

    fix: button documentation centered

--- dashboard/app.py
+++ dashboard/app.py
@@ -52,11 +52,10 @@ hide_st_style = """
                     color: #fff;
                     background-color: #206b82;
                     text-decoration: none;
-                    font-weight: bold;
                     text-align: center;
                     background-image: none;
-                    padding: 8px 12px;
-                    font-size: 14px;
+                    padding: 12px;
+                    font-size: 16px;
                     border-radius: 6px;
                 }
                 a.btn:hover{
@@ -588,20 +587,23 @@ if selected == "About":
 
     st.markdown('---')
     documentation_button = '''
-    <a href="https://www.pessini.me/moby-bikes/" target="_blank" class="btn">
-        <!-- Box arrow up-right -->
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
-            <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-        </svg>
-        <span style="vertical-align: baseline;">&nbspRead full documentation</span>
-    </a>
+    <div style="position: static;margin-bottom:10px; text-align:center;">
+        <a href="https://www.pessini.me/moby-bikes/" target="_blank" class="btn">
+            <!-- Box arrow up-right -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+                <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+            </svg>
+            <span style="vertical-align: baseline;">&nbspRead full documentation</span>
+        </a>
+    </div>
     '''
     st.markdown(documentation_button, unsafe_allow_html=True)
     # st.write("""
     #          [Read full documentation](https://www.pessini.me/moby-bikes/)
     #          """)
     # https://whimsical.com/design-docs-moby-bikes-operations-optimization-3RJyNyq2NHe8rPGzGZjrje
+    st.markdown("<div>&nbsp;</div>", unsafe_allow_html=True)
 
 footer_github = """<div style='position: absolute; padding-top: 100px; width:100%;'>
 <img title="GitHub Mark" src="https://github.com/pessini/avian-flu-wild-birds-ireland/blob/main/img/GitHub-Mark-64px.png?raw=true" 
