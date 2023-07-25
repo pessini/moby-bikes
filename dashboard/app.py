10527a27ab2ab9a6c4a2e3ca0a9108f54b06e1dc
Leandro Pessini <leandro@pessini.me>
2023-07-25 19:42:47 -0300

    fix: path to model pickle file

--- dashboard/app.py
+++ dashboard/app.py
@@ -101,7 +101,8 @@ def host_is_local(hostname, port=None):
 if socket.gethostname() == 'Leandros-MacBook-Air.local': # my mac
     APP_PATH = '/Users/pessini/Dropbox/Data-Science/moby-bikes/dashboard/'
 else: # remote
-    APP_PATH = '/app/moby-bikes/dashboard/'
+    # APP_PATH = '/app/moby-bikes/dashboard/'
+    APP_PATH = '/dashboard/'
 
 def format_rental_duration(minutes):
     return timedelta(minutes=float(minutes)).__str__()
